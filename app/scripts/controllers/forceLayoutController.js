7225ed8dbdce18ea6a5ee9fb046fe45e50417479
Khaled Osman <khaled.osmaan@gmail.com>
2015-07-06 16:46:25 +0200

    stop timeout

--- app/scripts/controllers/forceLayoutController.js
+++ app/scripts/controllers/forceLayoutController.js
@@ -5,7 +5,8 @@ app.controller('forceLayoutController', function($scope, $timeout, $interval){
 		$scope.shared.url='graph.json'; 
 		console.log($scope.shared.url);
 	}, 3000,2);*/
-	$timeout(function(){
+/*	$timeout(function(){
 		$scope.shared.url='graph2.json'; 
-	}, 5000);
+	}, 5000); 
+*/
 });
--- app/scripts/directives/forceLayout.js
+++ app/scripts/directives/forceLayout.js
@@ -160,10 +160,10 @@ function tick() {
     .attr("x1", function(d) { return d.source.x; })
     .attr("y1", function(d) { return d.source.y; })
     .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+    .attr("y2", function(d) { return d.target.y; })
 
   node
-    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 }
 
 function color(d) {
