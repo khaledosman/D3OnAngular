98ec259abcde7fed088a46ace30c89887c346da4
Khaled Osman <khaled.osmaan@gmail.com>
2015-07-10 11:46:01 +0200

    changing json url from controller

--- app/scripts/controllers/forceLayoutController.js
+++ app/scripts/controllers/forceLayoutController.js
@@ -1,3 +1,4 @@
-app.controller('forceLayoutController', function($scope) {
+app.controller('forceLayoutController', function($scope, $timeout) {
 	$scope.shared = { url:'graph2.json'};
+	//$timeout(function() {$scope.shared.url = 'graph3.json';}, 2000);
 });
