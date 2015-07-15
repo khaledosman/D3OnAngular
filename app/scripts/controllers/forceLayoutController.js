5e791b63243f19fbe75a5e6d3a0073dfd65ae191
itonics-tbeauvais <thomas.beauvais@itonics.de>
2015-07-15 14:08:03 +0200

    updated the forceLayoutController.js per our discussion

--- app/scripts/controllers/forceLayoutController.js
+++ app/scripts/controllers/forceLayoutController.js
@@ -1,7 +1,7 @@
 /**
  * Array.prototype.[method name] allows you to define/overwrite an objects method
  * needle is the item you are searching for
- * this is a special variable that refers to "this" instance of an Array.
+ * this is a special variable tat refers to "this" instance of an Array.
  * returns true if needle is in the array, and false otherwise
  */
 
@@ -13,41 +13,13 @@ app.controller('forceLayoutController', function($scope, $timeout, $http) {
 
 	$scope.links = [];
 
-	$scope.nodes.contains = function(name) {
-		for (var i in this) {
-			if (this[i].name === name) return true;
-		}
-	};
-	/*$scope.nodes.indexOf = function(name) {
-		console.log("This", this);
-		console.log("name", name);
-		for (var i in this) {
-			if (this[i].name === name) return i;
-			else console.log('fail');
-		}
-	};*/
+	$scope.nameToIdMap = {};
 
 	$scope.nodes.getNode = function(name) {
-		for (var i in this) {
-			if (this[i].name === name) return this[i];
-		}
+		return $scope.nodes[$scope.nameToIdMap[name]]
 	};
 
-	$scope.idCounter = (function() {
-		var privateCounter = 0;
-
-		function addOne() {
-			privateCounter++;
-		}
-		return {
-			increment: function() {
-				addOne();
-			},
-			value: function() {
-				return privateCounter;
-			}
-		};
-	})();
+	$scope.counter = 0;
 
 	// Simple GET request example :
 	$http.get('http://localhost:8080/model/events').success(function(data, status, headers, config) {
@@ -62,52 +34,41 @@ app.controller('forceLayoutController', function($scope, $timeout, $http) {
 	});
 	//$timeout(function() {$scope.shared.url = 'graph2.json';}, 7000);
 
+	$scope.createOrFindNode = function(name) {
+		var node = $scope.nodes.getNode(name);
+
+		if (!node) {
+			node = {
+				name: name,
+				id: $scope.counter++,
+				group: Math.floor(Math.random() * (8 - 1 + 1)) + 1,
+			};
+
+			$scope.nameToIdMap[name] = node.id;
+			$scope.nodes.push(node);
+		}
+
+		return node;
+	};
+
 	$scope.generateGraphFromData = function(data) {
 		data.forEach(function(d) {
 			if (d.type === "ADDED") {
-				var subject = d.newAssertion[1].split('#')[1];
-				var object = d.newAssertion[3].split('#')[1];
-				var predicate = d.newAssertion[2].split('#')[1];
 				var namespace = d.newAssertion[0].split('/')[3];
+				var subject = d.newAssertion[1].split('#')[1];
+				var predicate = d.newAssertion[2].split('#')[1];
+				var object = d.newAssertion[3].split('#')[1];
 				//console.log("name space -->", namespace);
 				//console.log("subject", subject);
 				//console.log("predicate", predicate);
 				//console.log("object", object);
 
-				var subjNode = {
-					name: "",
-					group: Math.floor(Math.random() * (8 - 1 + 1)) + 1,
-				};
-
-				var objNode = {
-					name: "",
-					group: Math.floor(Math.random() * (8 - 1 + 1)) + 1,
-				};
-
-				if (!$scope.nodes.contains(subject)) {
-					subjNode.id = $scope.idCounter.value();
-					subjNode.name = subject;
-					$scope.idCounter.increment();
-					//console.log(subjNode);
-					$scope.nodes.push(subjNode);
-				} else {
-					subjNode = $scope.nodes.getNode(subject);
-				}
-
-				if (!$scope.nodes.contains(object)) {
-					objNode.id = $scope.idCounter.value();
-					objNode.name = object;
-					$scope.idCounter.increment();
-					//console.log(objNode);
-					$scope.nodes.push(objNode);
-				} else {
-					objNode = $scope.nodes.getNode(object);
-				}
-
+				var subjectNode = $scope.createOrFindNode(subject);
+				var objectNode = $scope.createOrFindNode(object);
 
 				var link = {
-					source: $scope.nodes.indexOf(subjNode),
-					target: $scope.nodes.indexOf(objNode),
+					source: subjectNode.id,
+					target: objectNode.id,
 					name: predicate,
 					value: Math.floor(Math.random() * (10 - 1 + 1)) + 1
 				};
@@ -117,11 +78,11 @@ app.controller('forceLayoutController', function($scope, $timeout, $http) {
 
 			}
 		});
-		console.log($scope.nodes);
-		console.log($scope.links);
+		//console.log($scope.nodes);
+		//console.log($scope.links);
 		$scope.links.forEach(function(link) {
-			if (link.source === link.target)
-				console.log("B-I-N-G-O");
+			//if (link.source === link.target)
+			//	console.log("B-I-N-G-O");
 		});
 	};
 });
